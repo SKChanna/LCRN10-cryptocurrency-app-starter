af590374b05c1f21f1e20793f45a4567ac61965f
Salman Khan <salman.channa@i2p.com.pk>
2021-12-05 16:40:10 +0500

    changed the name of the app

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -41,43 +41,36 @@ const TabBarCustomButton = ({children, onPress}) => {
   )
 }
 
+const TabBarIcon = ({focused, icon, text}) => (
+  <View style={{ alignItems: 'center', justifyContent: 'center'}}>
+    <Image
+      source={icons.home}
+      resizeMode="contain"
+      style={{
+        width: 30,
+        height: 30,
+        tintColor: focused ? COLORS.primary : COLORS.black
+      }}
+    />
+    <Text style={{ color: focused ? COLORS.primary : COLORS.black, ...FONTS.body5}}>
+      HOME
+    </Text>
+  </View>
+)
+
 const Tabs = () => {
     return (
         <Tab.Navigator
           tabBarOptions={{
             showLabel: false,
-            style: {
-                position: 'absolute',
-                bottom: 0,
-                left: 0,
-                right: 0,
-                elevation: 0,
-                backgroundColor: COLORS.white,
-                borderTopColor: 'transparent',
-                height: 100
-              }
+            style: styles.tabNavigatorStyle
           }}
         >
             <Tab.Screen
                 name="Home"
                 component={Home}
                 options={{
-                  tabBarIcon: ({focused}) => (
-                    <View style={{ alignItems: 'center', justifyContent: 'center'}}>
-                      <Image
-                        source={icons.home}
-                        resizeMode="contain"
-                        style={{
-                          width: 30,
-                          height: 30,
-                          tintColor: focused ? COLORS.primary : COLORS.black
-                        }}
-                      />
-                      <Text style={{ color: focused ? COLORS.primary : COLORS.black, ...FONTS.body5}}>
-                        HOME
-                      </Text>
-                    </View>
-                  )
+                  tabBarIcon: ({focused}) => TabBarIcon({focused, icon: icons.home, text: 'Home'})
                 }}
             />
             <Tab.Screen
@@ -181,7 +174,17 @@ const styles = StyleSheet.create({
         shadowRadius: 3.84,
 
         elevation: 5
-    }
+    },
+  tabNavigatorStyle: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    elevation: 0,
+    backgroundColor: COLORS.white,
+    borderTopColor: 'transparent',
+    height: 100
+  }
 })
 
 export default Tabs;
